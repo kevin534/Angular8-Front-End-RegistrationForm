e105f829737c39379d33542d0e2416241d9f6a54
kevin <60352713+kevin534@users.noreply.github.com>
2021-11-05 11:12:12 +0100

    Imit Comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginsuccessComponent } from './loginsuccess/loginsuccess.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    LoginsuccessComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+// ng generate class User
+export class User {
+    id!:number;
+    emailId!:string;
+    userName!:string;
+    password!:string;
+    constructor(){}
+}
